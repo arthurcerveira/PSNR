f9a955550568b4609b34488545a0f296f401c700
arthurcerveira <aacerveira@inf.ufpel.edu.br>
2020-08-03 10:51:02 -0300

    Optimize how the videos are read

--- psnr.py
+++ psnr.py
@@ -1,34 +1,38 @@
 import numpy as np
 
 
+MAX_VALUE = 1020
+
+
 class VideoCaptureYUV(object):
-    def __init__(self, filename, resolution):
-        self.width, self.height = resolution
+    def __init__(self, filename, resolution, bitdepth):
         self.file = open(filename, 'rb')
+        self.width, self.height = resolution
+        self.uv_width = self.width // 2
+        self.uv_height = self.height // 2
+        self.bitdepth = bitdepth
 
-    def read_frame(self, bitdepth):
-        uv_width = self.width // 2
-        uv_height = self.height // 2
-
-        Y = self.read_channel(self.height, self.width, bitdepth)
-        U = self.read_channel(uv_height, uv_width, bitdepth)
-        V = self.read_channel(uv_height, uv_width, bitdepth)
+    def read_frame(self):
+        Y = self.read_channel(self.height, self.width)
+        U = self.read_channel(self.uv_height, self.uv_width)
+        V = self.read_channel(self.uv_height, self.uv_width)
 
         return Y, U, V
 
-    def read_channel(self, height, width, bitdepth):
-        channel = np.zeros((height, width), np.uint8)
+    def read_channel(self, height, width):
+        channel_len = height * width
+        shape = (height, width)
 
-        for m in range(height):
-            for n in range(width):
-                if bitdepth == 8:
-                    pel8bit = int.from_bytes(self.file.read(
-                        1), byteorder='little', signed=False)
-                    channel[m, n] = np.uint8(pel8bit)
-                elif bitdepth == 10:
-                    pel10bit_v = int.from_bytes(self.file.read(
-                        2), byteorder='little', signed=False)
-                    channel[m, n] = np.uint8((pel10bit_v + 2) / 4)
+        if self.bitdepth == 8:
+            raw = self.file.read(channel_len)
+            channel_8bits = np.frombuffer(raw, dtype=np.uint8)
+            channel = np.array(channel_8bits, dtype=np.uint16) << 2  # Convert 8bits to 10 bits 
+
+        elif self.bitdepth == 10:
+            raw = self.file.read(2 * channel_len)  # Read 2 bytes for every pixel
+            channel = np.frombuffer(raw, dtype=np.uint16)
+        
+        channel = channel.reshape(shape)
 
         return channel
 
@@ -36,7 +40,7 @@ class VideoCaptureYUV(object):
         self.file.close()
 
 
-def psnr_channel(original, encoded, max_value):
+def psnr_channel(original, encoded):
     # Convert frames to double
     original = np.array(original, dtype=np.double)
     encoded = np.array(encoded, dtype=np.double)
@@ -45,13 +49,13 @@ def psnr_channel(original, encoded, max_value):
     mse = np.mean((original - encoded) ** 2)
 
     # PSNR in dB
-    psnr = 10 * np.log10((max_value * max_value) / (mse))
+    psnr = 10 * np.log10((MAX_VALUE * MAX_VALUE) / mse)
     return psnr, mse
 
 
-def calculate_psnr(original, encoded, resolution, frames, max_value=255):
-    original_video = VideoCaptureYUV(original, resolution)
-    encoded_video = VideoCaptureYUV(encoded, resolution)
+def calculate_psnr(original, encoded, resolution, frames, original_bitdepth, encoded_bitdepth):
+    original_video = VideoCaptureYUV(original, resolution, original_bitdepth)
+    encoded_video = VideoCaptureYUV(encoded, resolution, encoded_bitdepth)
 
     psnr_y_array = list()
     psnr_u_array = list()
@@ -59,16 +63,16 @@ def calculate_psnr(original, encoded, resolution, frames, max_value=255):
     mse_array = list()
 
     for frame in range(frames):
-        original_y, original_u, original_v = original_video.read_frame(bitdepth=8)
-        encoded_y, encoded_u, encoded_v = encoded_video.read_frame(bitdepth=8)
+        original_y, original_u, original_v = original_video.read_frame()
+        encoded_y, encoded_u, encoded_v = encoded_video.read_frame()
 
-        psnr_y, mse_y = psnr_channel(original_y, encoded_y, max_value)
+        psnr_y, mse_y = psnr_channel(original_y, encoded_y)
         psnr_y_array.append(psnr_y)
 
-        psnr_u, mse_u = psnr_channel(original_u, encoded_u, max_value)
+        psnr_u, mse_u = psnr_channel(original_u, encoded_u)
         psnr_u_array.append(psnr_u)
 
-        psnr_v, mse_v = psnr_channel(original_v, encoded_v, max_value)
+        psnr_v, mse_v = psnr_channel(original_v, encoded_v)
         psnr_v_array.append(psnr_v)
 
         mse = (4 * mse_y + mse_u + mse_v) / 6  # Weighted MSE
@@ -85,18 +89,23 @@ def calculate_psnr(original, encoded, resolution, frames, max_value=255):
 
     # Calculate YUV-PSNR based on average MSE
     mse_yuv = np.average(mse_array)
-    psnr_yuv = 10 * np.log10((max_value * max_value) / (mse_yuv))
+    psnr_yuv = 10 * np.log10((MAX_VALUE * MAX_VALUE) / mse_yuv)
 
     return psnr_y, psnr_u, psnr_v, psnr_yuv
 
 
 if __name__ == "__main__":
-    original_video = "BQSquare_416x240_60.yuv"
-    encoded_video = "BQSquare-Encoded-LD-8bits.yuv"
+    original_video = "RaceHorses_416x240_30.yuv"
+    encoded_video = "RaceHorses-Decoded-LD-10bits.yuv"
     resolution = (416, 240)
+    frames = 5
+    original_bitdepth = 8
+    encoded_bitdepth = 10
 
     psnr_y, pnsr_u, psnr_v, psnr_yuv = calculate_psnr(
-        original_video, encoded_video, resolution, 5)
+        original_video, encoded_video, resolution, 
+        frames, original_bitdepth, encoded_bitdepth
+    )
 
     print(f'Y-PSNR: {psnr_y:.4f}dB')
     print(f'U-PSNR: {pnsr_u:.4f}dB')
